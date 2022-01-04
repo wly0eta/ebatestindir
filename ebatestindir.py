a0ec85d7a32fa011c8270085f50e5c3bd9b1c347
Ozan Bıçakçı <72266257+PortPolly@users.noreply.github.com>
2022-01-04 17:58:03 +0300

    Update ebatestindir.py

--- ebatestindir.py
+++ ebatestindir.py
@@ -1,12 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+print(
+"""
+------------------------------------
+Test Numarası ----------> kaçıncı teste kadar indirmek istediğiniz örn: 11 
+------------------------------------
+5. sınıftan mezuna kadar var 656 to 727
+""")
 testnum = input("Test sayısı : ")
+
+
 url = "https://odsgm.meb.gov.tr/www/11-sinif-fizik-kazanim-testleri/icerik/704"
 response = requests.get(url)
 html = response.content
 soup = BeautifulSoup(html,"html.parser")
 linkler = soup.find_all("a",href=True)
-
 num = 0
 for link in linkler:
     if(link['href'].startswith("/destekmateryal/pdf/11kt/") and num < int(testnum)):
